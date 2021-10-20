05cba55e18424fc9a7771860190b9998393a25ae
wangsongc <wangsongc@foxmail.com>
2021-10-20 14:16:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,151 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <Swiper /> -->
+    <!-- <example00 /> -->
+    <!-- <example01 /> -->
+    <example02 />
+    <!-- <example03 />
+    <example04 />
+    <example05 />
+    <example06 />
+    <example07 />
+    <example08 />
+    <example09 />
+    <example10 />
+    <example11 />
+    <example12 />
+    <example13 />
+    <example14 />
+    <example15 />
+    <example16 />
+    <example17 />
+    <example18 />
+    <example19 />
+    <example20 />
+    <example21 />
+    <example22 />
+    <example23 />
+    <example24 />
+    <example25 />
+    <example26 />
+    <example27 />
+    <example28 />
+    <example29 />
+    <example30 />
+    <example31 />
+    <example32 />
+    <example33 />
+    <example34 />
+    <example35 />
+    <example36 />
+    <example37 />
+    <example38 />
+    <example39 />
+    <example40 /> -->
+  </div>
+</template>
+
+<script>
+// import Swiper from "./components/Swiper.vue";
+// import example00 from './components/examples/00-typescript-composition-api.vue'
+// import example01 from "./components/examples/01-default.vue";
+import example02 from "./components/examples/02-navigation.vue";
+// import example03 from "./components/examples/03-pagination.vue";
+// import example04 from "./components/examples/04-pagination-dynamic.vue";
+// import example05 from "./components/examples/05-pagination-progress.vue";
+// import example06 from "./components/examples/06-pagination-fraction.vue";
+// import example07 from "./components/examples/07-pagination-custom.vue";
+// import example08 from "./components/examples/08-scrollbar.vue";
+// import example09 from "./components/examples/09-vertical.vue";
+// import example10 from "./components/examples/10-space-between.vue";
+// import example11 from "./components/examples/11-slides-per-view.vue";
+// import example12 from "./components/examples/12-slides-per-view-auto.vue";
+// import example13 from "./components/examples/13-centered.vue";
+// import example14 from "./components/examples/14-centered-auto.vue";
+// import example15 from "./components/examples/15-freemode.vue";
+// import example16 from "./components/examples/16-scroll-container.vue";
+// import example17 from "./components/examples/17-slides-per-column.vue";
+// import example18 from "./components/examples/18-nested.vue";
+// import example19 from "./components/examples/19-grab-cursor.vue";
+// import example20 from "./components/examples/20-infinite-loop.vue";
+// import example21 from "./components/examples/21-infinite-loop-with-slides-per-group.vue";
+// import example22 from "./components/examples/22-effect-fade.vue";
+// import example23 from "./components/examples/23-effect-cube.vue";
+// import example24 from "./components/examples/24-effect-coverflow.vue";
+// import example25 from "./components/examples/25-effect-flip.vue";
+// import example26 from "./components/examples/26-keyboard-control.vue";
+// import example27 from "./components/examples/27-mousewheel-control.vue";
+// import example28 from "./components/examples/28-autoplay.vue";
+// import example29 from "./components/examples/29-dynamic-slides.vue";
+// import example30 from "./components/examples/30-thumbs-gallery.vue";
+// import example31 from "./components/examples/31-multiple-swipers.vue";
+// import example32 from "./components/examples/32-rtl.vue";
+// import example33 from "./components/examples/33-parallax.vue";
+// import example34 from "./components/examples/34-lazy-load-images.vue";
+// import example35 from "./components/examples/35-responsive-breakpoints.vue";
+// import example36 from "./components/examples/36-autoheight.vue";
+// import example37 from "./components/examples/37-zoom.vue";
+// import example38 from "./components/examples/38-virtual-slides.vue";
+// import example39 from "./components/examples/39-slideable-menu.vue";
+// import example40 from "./components/examples/40-change-direction.vue";
+
+export default {
+  name: "App",
+  components: {
+    // Swiper,
+    // example00,
+    // example01,
+    example02,
+    // example03,
+    // example04,
+    // example05,
+    // example06,
+    // example07,
+    // example08,
+    // example09,
+    // example10,
+    // example11,
+    // example12,
+    // example13,
+    // example14,
+    // example15,
+    // example16,
+    // example17,
+    // example18,
+    // example19,
+    // example20,
+    // example21,
+    // example22,
+    // example23,
+    // example24,
+    // example25,
+    // example26,
+    // example27,
+    // example28,
+    // example29,
+    // example30,
+    // example31,
+    // example32,
+    // example33,
+    // example34,
+    // example35,
+    // example36,
+    // example37,
+    // example38,
+    // example39,
+    // example40
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
